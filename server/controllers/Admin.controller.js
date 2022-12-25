28e4cb7676a37fdead0c089693ed24832cdd6b47
DaDDy <blacklistuser420@gmail.com>
2022-12-25 19:46:04 +0630

    admin signup ui finish

--- server/controllers/Admin.controller.js
+++ server/controllers/Admin.controller.js
@@ -1,9 +1,17 @@
 const path = require("path");
 const httpGetAdmin = (req, res) => {
-  res.sendFile(
+  return res.sendFile(
     path.join(__dirname, "..", "..", "client", "Admin", "adminPenel.html")
   );
 };
+
+const httpGetAdminSignup = (req, res) => {
+  return res.sendFile(
+    path.join(__dirname, "..", "..", "client", "Admin", "signup.html")
+  );
+};
+
 module.exports = {
   httpGetAdmin,
+  httpGetAdminSignup,
 };
--- server/routes/Admin.router.js
+++ server/routes/Admin.router.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { httpGetAdmin } = require("../controllers/Admin.controller");
+const {
+  httpGetAdmin,
+  httpGetAdminSignup,
+} = require("../controllers/Admin.controller");
 const adminRouter = express.Router();
 
 adminRouter.get("/", httpGetAdmin);
+adminRouter.get("/signup", httpGetAdminSignup);
 
 module.exports = adminRouter;
